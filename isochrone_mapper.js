f35f4de8fcb61746e55536710e85447fd0fefb75
Unknown <27.gadda@mailbox.org>
2018-10-23 11:07:02 +0200

    Added Isochrone API Mapping

---
+++ isochrone_mapper.js
@@ -0,0 +1,77 @@
+
+let amountOfBuckets
+let totalTime
+let contoursColors
+
+exports.map = function (response, time, buckets, colors) {
+  amountOfBuckets = buckets
+  totalTime = time
+  try {
+    contoursColors = colors.split(',')
+  } catch (e) {
+    contoursColors = []
+  }
+  contoursColors = colors
+  let polygons = response.polygons
+  let mapboxIsoRes = {
+    'features': getFeatures(polygons),
+    'type': 'FeatureCollection'
+  }
+  return mapboxIsoRes
+}
+
+function getFeatures (polygons, colors) {
+  let features = []
+  polygons.map(polygon => {
+    features.push(createFeature(polygon))
+  })
+  return features
+}
+
+function createFeature (polygon) {
+  let color = getContourColor(polygon)
+  let feature = {
+    'properties': {
+      'contour': getContourMinutes(polygon),
+      'color': color,
+      'opacity': 0.33,
+      'fill': color,
+      'fill-opacity': 0.33,
+      'fillColor': color,
+      'fillOpacity': 0.33
+    },
+    'type': 'Feature',
+    'geometry': {
+      'coordinates': getCoordinates(polygon),
+      'type': 'Polygon'
+    }
+  }
+  return feature
+}
+
+function getContourColor (polygon) {
+  let bucket = polygon.properties.bucket
+  let color
+  try {
+    color = contoursColors[bucket]
+  } catch (e) {
+    color = 'ff0000'
+  }
+  color = '#' + color
+  return color
+}
+
+function getContourMinutes (polygon) {
+  let bucket = polygon.properties.bucket
+  let fraction = (bucket + 1) / (amountOfBuckets)
+  let time = fraction * totalTime
+  let minutes = Math.round(time / 60)
+  console.log(bucket, amountOfBuckets, fraction, time, minutes)
+
+  return minutes
+}
+
+function getCoordinates (polygon) {
+  let coordinates = polygon.geometry.coordinates
+  return coordinates
+}
